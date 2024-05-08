de2a93689ef764f2c45e873e30e4ecebf1ec910e
botslith <paleciosz@gmail.com>
2024-05-08 19:37:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const keep_alive = require('./keep_alive.js')
 
 const client = new Discord.Client();
 
+let status = [
+  {
+    name: 'Under Ctrl',
+    type: ActivityType.Streaming,
+    url: 'https://youtu.be/2lgEZB4eEF0?si=Q7c1CE_x4F132OZR',
+  },
+]
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
