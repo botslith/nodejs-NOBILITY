61ecbf63db5772d44497a38b3038671d3173128f
botslith <paleciosz@gmail.com>
2024-05-08 18:47:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-const Discord = require('discord.js'); //import client from discord
+const { Discord, ActivityType } = require('discord.js'); //import client from discord
 const keep_alive = require('./keep_alive.js')
 
 const client = new Discord.Client();
 
+let status = [
+  {
+    name: 'Under Ctrl',
+    type: ActivityType.Streaming,
+    url: 'https://youtu.be/2lgEZB4eEF0?si=cOLDecQGHarYx83_',
+  },
+]
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-    // check if message isn't from us
-    if (msg.author == client.user) {
-      return;
-    }
-    else if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
-    else {
-        msg.reply(msg.content);
-    }
 });
 
 client.login(process.env.TOKEN); //login bot using token
